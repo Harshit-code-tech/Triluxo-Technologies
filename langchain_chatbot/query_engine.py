dbc6b095875812f2c7aab68b7c061b0a52ae3b66
Harshit Ghosh <harshitghosh6@gmail.com>
2025-02-11 01:12:44 +0530

    deploy update

--- langchain_chatbot/query_engine.py
+++ langchain_chatbot/query_engine.py
@@ -1,4 +1,4 @@
-from langchain.vectorstores import Chroma
+from langchain_community.vectorstores import Chroma
 from langchain_huggingface import HuggingFaceEmbeddings
 import os
 import tensorflow as tf
